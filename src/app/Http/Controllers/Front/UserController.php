706d09c64aa0547c5e92ad49f6028650a4499b34
shuntaro-fukuta <shuntaro.fkt@gmail.com>
2020-12-07 21:06:25 +0900

    処理対象が本人のものかどうかの確認処理をミドルウェアで実装

--- src/app/Http/Controllers/Front/UserController.php
+++ src/app/Http/Controllers/Front/UserController.php
@@ -12,35 +12,18 @@ class UserController extends Controller
 {
     public function show(User $user)
     {
-        if (!$this->isHimself($user)) {
-            abort(404);
-        }
-
         return view('front.user.show', ['user' => $user]);
     }
 
     public function edit(User $user)
     {
-        if (!$this->isHimself($user)) {
-            abort(404);
-        }
-
         return view('front.user.edit', ['user' => $user]);
     }
 
     public function update(UpdateUserRequest $request, User $user)
     {
-        if (!$this->isHimself($user)) {
-            abort(404);
-        }
-
         $user->fill($request->all())->save();
 
         return redirect(route('user.show', $user->id));
-    }
-
-    private function isHimself(User $user)
-    {
-        return ($user->id === auth()->user()->id);
     }
 }
---
+++ src/app/Http/Middleware/Identification.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+use App\Models\User;
+
+class Identification
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        $targetUser = $request->route()->parameter('user');
+        if ($targetUser->id !== auth()->user()->id) {
+            abort(404);
+        }
+
+        return $next($request);
+    }
+}
