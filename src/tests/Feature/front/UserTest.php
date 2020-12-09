8c079a097275066e84d0b92897a0d973294ba7b3
shuntaro-fukuta <shuntaro.fkt@gmail.com>
2020-12-10 00:33:13 +0900

    ユーザーの 詳細/編集 機能のテストを実装

---
+++ src/tests/Feature/front/UserTest.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace Tests\Feature\front;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+use App\Models\User;
+
+class UserTest extends TestCase
+{
+    protected $loggedInUser;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->loggedInUser = User::factory(1)->create()->first();
+        $this->actingAs($this->loggedInUser);
+    }
+
+    /**
+     * @test
+     */
+    public function shouldDisplayUserPage()
+    {
+        $response = $this->get('/user/' . $this->loggedInUser->id);
+
+        $response->assertStatus(200);
+    }
+
+    /**
+     * @test
+     */
+    public function shouldDisplayUserEditPage()
+    {
+        $response = $this->get('/user/' . $this->loggedInUser->id . '/edit');
+
+        $response->assertStatus(200);
+    }
+
+    /**
+     * @test
+     */
+    public function shouldEditUser()
+    {
+        $response = $this->post('/user/' . $this->loggedInUser->id . '/edit', [
+            'name' => 'updated',
+            'email' => 'updated@example.com',
+            'address' => 'updated',
+            'phone_number' => '99999999999',
+            'password' => 'updated!!!',
+        ]);
+
+        $updatedUser = User::find($this->loggedInUser->id);
+
+        $this->assertEquals($updatedUser->name, 'updated');
+        $this->assertEquals($updatedUser->email, 'updated@example.com');
+        $this->assertEquals($updatedUser->address, 'updated');
+        $this->assertEquals($updatedUser->phone_number, '99999999999');
+        $this->assertEquals($updatedUser->password, 'updated!!!');
+
+        $response->assertStatus(302);
+    }
+
+    /**
+     * @test
+     */
+    public function shouldNotEditOtherUser()
+    {
+        $response = $this->post('/user/999/edit', [
+            'name' => 'updated',
+            'email' => 'updated@example.com',
+            'address' => 'updated',
+            'phone_number' => '99999999999',
+            'password' => 'updated!!!',
+        ]);
+
+        $response->assertStatus(404);
+    }
+}
