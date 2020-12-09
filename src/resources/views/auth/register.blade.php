9c479f4511a9af4cb509b0cc3996902fc042361b
shuntaro-fukuta <shuntaro.fkt@gmail.com>
2020-12-09 23:20:07 +0900

    レイアウトの調整

--- src/resources/views/auth/register.blade.php
+++ src/resources/views/auth/register.blade.php
@@ -1,77 +1,96 @@
-@extends('layouts.app')
+@extends('front.layouts.base')
+
+@section('title', '会員登録')
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Register') }}</div>
+  <h2 class="text-center my-3">会員登録</h2>
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('register') }}">
-                        @csrf
+  <div class="card col-7 mx-auto mt-2">
+    <form method="post">
+      @csrf
 
-                        <div class="form-group row">
-                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
-
-                                @error('name')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Register') }}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
+      <div class="row mt-3">
+        <div class="col-8 mx-auto d-flex my-2 font-weight-bold">
+          <label for="name" class="col-5">氏名</label>
+          <input id="name" type="text" name="name" value="{{ old('name') }}">
         </div>
-    </div>
+      </div>
+      @if($errors->has('name'))
+      <div class="row">
+        <div class="error col-8 mx-auto text-center">
+          <p>{{ $errors->first('name') }}</p>
+        </div>
+      </div>
+      @endif
+
+      <div class="row">
+        <div class="col-8 mx-auto d-flex my-2 font-weight-bold">
+          <label for="email" class="col-5">メールアドレス</label>
+          <input id="email" type="text" name="email" value="{{ old('email') }}">
+        </div>
+      </div>
+      @if($errors->has('email'))
+      <div class="row">
+        <div class="error col-8 mx-auto text-center">
+          <p>{{ $errors->first('email') }}</p>
+        </div>
+      </div>
+      @endif
+
+      <div class="row">
+        <div class="col-8 mx-auto d-flex my-2 font-weight-bold">
+          <label for="address" class="col-5">住所</label>
+          <input id="address" type="text" name="address" value="{{ old('address') }}">
+        </div>
+      </div>
+      @if($errors->has('address'))
+      <div class="row">
+        <div class="error col-8 mx-auto text-center">
+          <p>{{ $errors->first('address') }}</p>
+        </div>
+      </div>
+      @endif
+
+      <div class="row">
+        <div class="col-8 mx-auto d-flex my-2 font-weight-bold">
+          <label for="phone_number" class="col-5">電話番号</label>
+          <input id="phone_number" type="text" name="phone_number" value="{{ old('phone_number') }}">
+        </div>
+      </div>
+      @if($errors->has('phone_number'))
+      <div class="row">
+        <div class="error col-8 mx-auto text-center">
+            <p>{{ $errors->first('phone_number') }}</p>
+        </div>
+      </div>
+      @endif
+
+      <div class="row">
+        <div class="col-8 mx-auto d-flex my-2 font-weight-bold">
+          <label for="password" class="col-5">パスワード</label>
+          <input id="password" type="password" name="password">
+        </div>
+      </div>
+      @if($errors->has('password'))
+      <div class="row">
+        <div class="error col-8 mx-auto text-center">
+            <p>{{ $errors->first('password') }}</p>
+        </div>
+      </div>
+      @endif
+
+      <div class="row">
+        <div class="col-8 mx-auto d-flex my-2 font-weight-bold">
+          <label for="password_confirmation" class="col-5">パスワード(確認用)</label>
+          <input id="password_confirmation" type="password" name="password_confirmation">
+        </div>
+      </div>
+
+      <div class="row">
+        <input type="submit" class="btn btn-secondary mx-auto mt-4 mb-3" value="登録">
+      </div>
+    </form>
+  </div>
 </div>
-@endsection
+@stop
