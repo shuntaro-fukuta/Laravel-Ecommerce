b88bcc30b3eb46bf2f9e830fa9e64215dc5d1594
s-fukuta <s-fukuta@murauchi.com>
2020-12-16 19:01:04 +0900

    カテゴリーの更新機能を実装

--- src/resources/views/back/category/show.blade.php
+++ src/resources/views/back/category/show.blade.php
@@ -16,6 +16,16 @@
         <th scope="row">Name:</th>
         <td>{{ $category->name }}</td>
       </tr>
+    </table>
+    <div class="row text-center">
+      <a href="{{ route('back.categories.edit', $category) }}">
+        <button class="btn btn-info mb-3">編集</button>
+      </a>
+    </div>
+
+    <a href="{{ route('back.categories.index') }}">
+      <button class="btn btn-dark mb-3">戻る</button>
+    </a>
   </div>
 </div>
 @endsection
