53f9212d9b410a3023446811502700cd6d0cf3c4
shuntaro-fukuta <shuntaro.fkt@gmail.com>
2020-12-19 21:04:23 +0900

    公開区分が非公開になっている場合は非表示

--- src/resources/views/front/top.blade.php
+++ src/resources/views/front/top.blade.php
@@ -7,20 +7,22 @@
   @foreach ($products->chunk(4) as $chunk)
     <div class="row my-3">
       @foreach ($chunk as $product)
-      <div class="card col-3">
-        <a class="text-dark" href="{{ route('products.show', $product) }}">
-          <div class="my-2 text-center">
-            @if ($product->image_url !== null)
-              <div class="mx-auto">
-                <img src="{{ $product->image_url }}" alt="">
+        @if ($product->is_published)
+          <div class="card col-3">
+            <a class="text-dark" href="{{ route('products.show', $product) }}">
+              <div class="my-2 text-center">
+                @if ($product->image_url !== null)
+                  <div class="mx-auto">
+                    <img src="{{ $product->image_url }}" alt="">
+                  </div>
+                @endif
+                {{ $product->name }}
+                <br>
+                ￥{{ number_format($product->price) }}
               </div>
-            @endif
-            {{ $product->name }}
-            <br>
-            ￥{{ number_format($product->price) }}
+            </a>
           </div>
-        </a>
-      </div>
+        @endif
       @endforeach
     </div>
   @endforeach
