e01b6415bc2446aac2c6689ecb503bc8d88b3be0
shuntaro-fukuta <shuntaro.fkt@gmail.com>
2020-12-19 19:15:29 +0900

    フロント側にカテゴリー一覧を表示

--- src/app/Http/Controllers/HomeController.php
+++ src/app/Http/Controllers/HomeController.php
@@ -5,14 +5,16 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 use App\Models\Back\Product;
+use App\Models\Back\Category;
 
 class HomeController extends Controller
 {
     public function top()
     {
         $products = Product::paginate(20);
+        $categories = Category::all();
 
-        return view('front.top', compact('products'));
+        return view('front.top', compact('products', 'categories'));
     }
 
     public function index()
--- src/resources/views/front/top.blade.php
+++ src/resources/views/front/top.blade.php
@@ -3,25 +3,43 @@
 @section('title', 'Laravel-Ecommerce')
 
 @section('content')
-<div class="container">
-  <div class="col-12 mx-auto">
-    @foreach ($products->chunk(4) as $chunk)
-      <div class="row my-3">
-        @foreach ($chunk as $product)
-        <div class="card col-3">
-          @if ($product->image_url !== null)
-            <img src="{{ $product->image_url }}" alt="">
-          @endif
-
-          {{ $product->name }}
-          <br>
-          ￥{{ number_format($product->price) }}
+  <div class="row">
+    <!-- side bar -->
+    <div class="col-2 bd-sidebar">
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="collapse navbar-collapse" id="navbarNavDropdown">
+          <ul class="navbar-nav flex-column">
+            <li class="nav-item">hoge</li>
+            @foreach ($categories as $category)
+              <li class="nav-item">{{ $category->name }}</li>
+            @endforeach
+          </ul>
         </div>
-        @endforeach
-      </div>
-    @endforeach
-  </div>
+      </nav>
+    </div>
 
-  {{ $products->links('pagination::bootstrap-4') }}
-</div>
+    <!-- main content -->
+    <div class="col-10">
+      @foreach ($products->chunk(4) as $chunk)
+        <div class="row my-3">
+          @foreach ($chunk as $product)
+          <div class="card col-3">
+            <div class="my-2 text-center">
+              @if ($product->image_url !== null)
+                <img src="{{ $product->image_url }}" alt="">
+              @endif
+
+              {{ $product->name }}
+              <br>
+              ￥{{ number_format($product->price) }}
+            </div>
+          </div>
+          @endforeach
+        </div>
+      @endforeach
+
+    {{ $products->links('pagination::bootstrap-4') }}
+    </div>
+
+  </div>
 @stop
